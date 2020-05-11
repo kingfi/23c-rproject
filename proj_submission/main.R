021bfc425f3d113fcd18d6408f26aa9eca521bb2
Travis James Smith <trajamsmith@gmail.com>
2020-05-10 20:42:57 -0400

    More main.R changes

--- proj_submission/main.R
+++ proj_submission/main.R
@@ -11,18 +11,22 @@ rm(list = ls())
 library(dplyr)
 library(mixtools)
 lib <- modules::use("./R")
-graphing <- lib$graphingß
+graphing <- lib$graphing
 
 # Import the by-part table as `table`
 load("./data/Global Party Survey by Party Stata V2_1_Apr_2020.RData")
-### THIS DATASET MEETS ALL REQUIRED DATASET STANDARDS --
-### IT CONTAINS OVER 150 COLUMNS, MANY LOGICAL AND MANY
-### CONTINUOUS, ACROSS 1043 OBSERVATIONSs
+# !!! THIS DATASET MEETS ALL REQUIRED DATASET STANDARDS --
+# !!! IT CONTAINS OVER 150 COLUMNS, MANY LOGICAL AND MANY CONTINUOUS, ACROSS 1043 OBSERVATIONSs
+# !!! ADDITIONAL POINT #1
 
 ## ----------------------------------------------------------------
 ## Important Project Notes
 ## ----------------------------------------------------------------
 
+# This project analyzes the Global Party Survey, an expert-led survey of political parties around
+# the globe. For full documentation surrounding both the survey and the dataset, please see the `/docs`
+# directory of this repository.
+#
 # Some background -- We intended to structure this project as though it were a professional R package.
 # See here: http://r-pkgs.had.co.nz/intro.html
 # Per the above, our intention was to have a library, stored in our `/R` directory, from which we would
@@ -32,10 +36,14 @@ load("./data/Global Party Survey by Party Stata V2_1_Apr_2020.RData")
 # our analysis from the individual vignette notebooks into this "long R script," but many of the analytic
 # steps are still bundled into our library files. If any issues arise when running this script, please
 # reach out to Travis (as he's responsible for the goofy structural choices)!
+# !!! ADDITIONAL POINT #10
+#
+# To quickly find any of the project requirements (including the ten additional points), search this
+# document for the following string: `#!!!`
 #
 # Additionally, in the same `proj_submission` directory as this script, we've included a written
 # document addressing one of the ethical issues explored in this dataset.
-### ADDITIONAL POINT #4
+# !!! ADDITIONAL POINT #4
 
 ###########################################################################
 ###########################################################################
@@ -74,6 +82,7 @@ obs
 # observed difference of 0.1167466
 
 # Running a permutation test for the null hypothesis
+# !!! REQUIRED ANALYSIS #2
 # num of trials
 N <- 10000
 # vector to store results
@@ -96,6 +105,7 @@ mean(Diffs) # average difference is -0.000858495
 
 # visual of the distribution of differences
 hist(Diffs, breaks = "Fd")
+# !!! REQURIED GRAPHICAL DISPLAY #2
 
 abline(v = obs, col = "blue")
 
@@ -112,6 +122,144 @@ pv.2t # 0.6359364
 # could get this observed discrepacncy in average ideology by chance. Hence there is insufficient
 # evidence against the null hypothesis.
 
+###########################################################################
+###########################################################################
+#
+#                   Party-Alignment Distribution
+#
+###########################################################################
+###########################################################################
+# More-detailed version (alignment_distribution.Rmd) can be found in vignettes folder
+
+# Let's look at the ideological alignments of all political parties globally and see if they adhere to some distribution.
+# We'll be looking specifically at the economic alignments of the parties along a left-right axis, as reported by local political experts.
+
+## ----------------------------------------------------------------
+## Without Influence Weighting
+## ----------------------------------------------------------------
+
+# A priori, having not looked at the data, it might be reasonable to assume that the counts follow
+# a normal distribution. In other words, we might assume that political parties tend to tack to the
+# center (especially after weighting for influence) and that there are far fewer parties to either extreme.
+
+# Let's test this naive assumption. We can calculate the mean and standard deviation from the data and
+# overlay it on our histogram:
+
+mu <- mean(table$V4_Scale, na.rm = TRUE)
+sigma <- sd(table$V4_Scale, na.rm = TRUE)
+
+hist(table$V4_Scale, probability = TRUE)
+curve(dnorm(x, mean = mu, sd = sigma), add = TRUE)
+
+# We can pretty clearly see from the graphs that the naive assumption is incorrect, but let's test that
+# hypothesis. We can calculate _expected_ counts for each of our ten bins by integrating over the distribution.
+# Then we can run a chi-square test to see if our _observed_ bins might likely have been drawn from the normal
+# distribution:
+
+data <- table$V4_Scale
+
+# Bin our original data (our histogram function did this for us above).
+observed <- 1:10
+for (i in 1:length(observed)) {
+  inds <- which((i - 1) <= data & data < i)
+  observed[i] <- length(inds)
+}
+
+density <- function(x) {
+  dnorm(x, mean = mu, sd = sigma)
+}
+
+# Get proportions of results by region of normal distribution.
+bins <- 1:10
+for (i in 1:length(bins)) {
+  bins[i] <- integrate(density, lower = (i - 1), upper = i)$value
+}
+
+# Calculate expected values.
+expected <- bins * sum(observed)
+chi_sq <- sum((observed - expected)^2 / expected)
+
+# We have ten bins and are measuring one variable, so DoF = 9
+pvalue <- pchisq(chi_sq, 9, lower.tail = FALSE)
+pvalue
+
+# Our p-value is very, very small, suggesting that there's a very, very small probability
+# that our observed data was drawn from a normal distribution. We'll probably need a more
+# complicated model to describe the data.
+
+## ----------------------------------------------------------------
+## Accounting for Party Influence
+## ----------------------------------------------------------------
+# To be included, time permitting...
+
+## ----------------------------------------------------------------
+## Finding a Bimodal Distribution
+## ----------------------------------------------------------------
+
+# So, this is a little outside the scope of the class, but our histogram really looks like it has
+# a bimodal underlying distribution. More specifically, it looks like we might want to try some kind
+# of mixture model, maybe a mix of two normal distributions. We can use a package to calculate a likely
+# mixture model (`mixtools`, using expectation-maximation), then we can run a chi-square test like above
+# to see how well the model fits:
+
+cleaned <- table$V4_Scale[!is.na(table$V4_Scale)]
+mix_mdl <- normalmixEM(cleaned)
+
+means <- mix_mdl$mu
+sigmas <- mix_mdl$sigma
+lambdas <- mix_mdl$lambda
+
+f1 <-
+  function(x) {
+    lambdas[1] * dnorm(x, mean = means[1], sd = sigmas[1])
+  }
+f2 <-
+  function(x) {
+    lambdas[2] * dnorm(x, mean = means[2], sd = sigmas[2])
+  }
+
+hist(table$V4_Scale, probability = TRUE)
+curve(f1, add = TRUE)
+curve(f2, add = TRUE)
+# !!! REQUIRED GRAPHICAL DISPLAY #3
+
+# To run a chi-square test, let's try sampling this time. Our lambda values give us the proportions
+# of the two constituent normal distributions and, therefore, the probability that any single sample
+# is drawn from each. So we can then sample from the two normal distributions probabilistically:
+
+samples <- 1:1000
+for (i in samples) {
+  if (runif(1) < lambdas[1]) {
+    # Sample from the first distribution
+    samples[i] <- rnorm(1, mean = means[1], sd = sigmas[1])
+  } else {
+    # Sample from the second distribution
+    samples[i] <- rnorm(1, mean = means[2], sd = sigmas[2])
+  }
+}
+
+# Drop any samples (<0) or (>10)
+samples <- samples[samples > 0 & samples < 10]
+hist(samples)
+
+# Bin the samples
+bins <- 1:10
+for (i in 1:length(bins)) {
+  inds <- which((i - 1) <= samples & samples < i)
+  bins[i] <- length(inds)
+}
+
+
+chi_sq <- sum((observed - bins)^2 / bins)
+pvalue <- pchisq(chi_sq, 9, lower.tail = FALSE)
+pvalue
+# !!! REQUIRED ANALYSIS #2
+
+# This p-value at least describes something in the realm of possibility, but it's still below
+# the threshold of what we would consider likely. There's only a 0.6% chance that our observed party
+# alignments came from this mixed distribution. This makes sense given that our sample size isn't that
+# large, and, even if it were, we're analyzing such an abstract, subjective set of values that modeling is
+# naturally very difficult.
 
 ###########################################################################
 ###########################################################################
@@ -182,6 +330,7 @@ graphing$contingency_table(cont,
   x_label = "Left   —   Social Ideology   —   Right",
   y_label = "Pluralist      —      Populist"
 )
+# !!! REQUIRED GRAPHICAL DISPLAY #4
 
 # This data is a little hard to interpret purely from this contingency table, but
 # we can eyeball a couple of things. Firstly, there are a lot of very socially conservative
@@ -190,6 +339,7 @@ graphing$contingency_table(cont,
 # "center-pluralism." In other words, there appear to be two peaks: "center-left, center-pluralist"
 # and "hard-right, hard-populist." Remember, however, that this is only true for _social_ ideology!
 # Let's look at the larger, ordinal contingency table for economic ideology:
+# !!! REQUIRED ANALYSIS #3
 
 cont <- table(econ_align_ord, pop_align_ord)
 graphing$contingency_table(cont,
@@ -200,6 +350,10 @@ graphing$contingency_table(cont,
 
 # There's no clear axis from one diagonal corner to another here,
 # like there was with our social ideology table.
+
+## ----------------------------------------------------------------
+## Scatterplot Analysis
+## ----------------------------------------------------------------
 
 # Let's repeat the above analysis with scatterplots and scalar values. This
 # might give us a more granular view of how these different ideological metrics are correlated:
@@ -223,6 +377,7 @@ graphing$scatterplot(
 # Our scatterplot shows axis means with dotted lines and a linear regression,
 # shown in red. Confirming our results above, there doesn't seem to be any correlation
 # between economic ideology and populism. Let's even run a correlation test as a sanity check:
+# !!! ADDTIONAL POINT #9
 
 # Create correlation scatterplot -- gives us a different metric
 # than the regression plot above.
@@ -238,147 +393,22 @@ graphing$corr_scatterplot(table,
 
 graphing$scatterplot(
   table,
-  econ_align_scale,
+  soc_align_scale,
   pop_align_scale,
   means = TRUE,
   regression = TRUE,
-  title = "Economic Ideology — Populism",
-  x_label = "Left   —   Economic Ideology   —   Right",
+  title = "Social Ideology — Populism",
+  x_label = "Left   —   Social Ideology   —   Right",
   y_label = "Pluralist      —      Populist"
 )
 
-###########################################################################
-###########################################################################
-#
-#                   Party-Alignment Distribution
-#
-###########################################################################
-###########################################################################
-# More-detailed version (alignment_distribution.Rmd) can be found in vignettes folder
+# This looks a lot more promising. Let's calculate the correlation coefficient for this one too:
 
-# Let's look at the ideological alignments of all political parties globally and see if they adhere to some distribution.
-# We'll be looking specifically at the economic alignments of the parties along a left-right axis, as reported by local political experts.
-
-## ----------------------------------------------------------------
-## Without Influence Weighting
-## ----------------------------------------------------------------
-
-# A priori, having not looked at the data, it might be reasonable to assume that the counts follow
-# a normal distribution. In other words, we might assume that political parties tend to tack to the
-# center (especially after weighting for influence) and that there are far fewer parties to either extreme.
-
-# Let's test this naive assumption. We can calculate the mean and standard deviation from the data and
-# overlay it on our histogram:
-
-mu <- mean(table$V4_Scale, na.rm = TRUE)
-sigma <- sd(table$V4_Scale, na.rm = TRUE)
-
-hist(table$V4_Scale, probability = TRUE)
-curve(dnorm(x, mean = mu, sd = sigma), add = TRUE)
-
-# We can pretty clearly see from the graphs that the naive assumption is incorrect, but let's test that
-# hypothesis. We can calculate _expected_ counts for each of our ten bins by integrating over the distribution.
-# Then we can run a chi-square test to see if our _observed_ bins might likely have been drawn from the normal
-# distribution:
-
-data <- table$V4_Scale
-
-# Bin our original data (our histogram function did this for us above).
-observed <- 1:10
-for (i in 1:length(observed)) {
-  inds <- which((i - 1) <= data & data < i)
-  observed[i] <- length(inds)
-}
-
-density <- function(x) {
-  dnorm(x, mean = mu, sd = sigma)
-}
-
-# Get proportions of results by region of normal distribution.
-bins <- 1:10
-for (i in 1:length(bins)) {
-  bins[i] <- integrate(density, lower = (i - 1), upper = i)$value
-}
-
-# Calculate expected values.
-expected <- bins * sum(observed)
-chi_sq <- sum((observed - expected)^2 / expected)
-
-# We have ten bins and are measuring one variable, so DoF = 9
-pvalue <- pchisq(chi_sq, 9, lower.tail = FALSE)
-pvalue
-
-# Our p-value is very, very small, suggesting that there's a very, very small probability
-# that our observed data was drawn from a normal distribution. We'll probably need a more
-# complicated model to describe the data.
-
-## ----------------------------------------------------------------
-## Accounting for Party Influence
-## ----------------------------------------------------------------
-
-## ----------------------------------------------------------------
-## Finding a Bimodal Distribution
-## ----------------------------------------------------------------
-
-# So, this is a little outside the scope of the class, but our histogram really looks like it has
-# a bimodal underlying distribution. More specifically, it looks like we might want to try some kind
-# of mixture model, maybe a mix of two normal distributions. We can use a package to calculate a likely
-# mixture model (`mixtools`, using expectation-maximation), then we can run a chi-square test like above
-# to see how well the model fits:
-
-cleaned <- table$V4_Scale[!is.na(table$V4_Scale)]
-mix_mdl <- normalmixEM(cleaned)
-
-means <- mix_mdl$mu
-sigmas <- mix_mdl$sigma
-lambdas <- mix_mdl$lambda
-
-f1 <-
-  function(x) {
-    lambdas[1] * dnorm(x, mean = means[1], sd = sigmas[1])
-  }
-f2 <-
-  function(x) {
-    lambdas[2] * dnorm(x, mean = means[2], sd = sigmas[2])
-  }
-
-hist(table$V4_Scale, probability = TRUE)
-curve(f1, add = TRUE)
-curve(f2, add = TRUE)
-
-# To run a chi-square test, let's try sampling this time. Our lambda values give us the proportions
-# of the two constituent normal distributions and, therefore, the probability that any single sample
-# is drawn from each. So we can then sample from the two normal distributions probabilistically:
-
-samples <- 1:1000
-for (i in samples) {
-  if (runif(1) < lambdas[1]) {
-    # Sample from the first distribution
-    samples[i] <- rnorm(1, mean = means[1], sd = sigmas[1])
-  } else {
-    # Sample from the second distribution
-    samples[i] <- rnorm(1, mean = means[2], sd = sigmas[2])
-  }
-}
-
-# Drop any samples (<0) or (>10)
-samples <- samples[samples > 0 & samples < 10]
-hist(samples)
-
-# Bin the samples
-bins <- 1:10
-for (i in 1:length(bins)) {
-  inds <- which((i - 1) <= samples & samples < i)
-  bins[i] <- length(inds)
-}
-
-
-chi_sq <- sum((observed - bins)^2 / bins)
-pvalue <- pchisq(chi_sq, 9, lower.tail = FALSE)
-pvalue
-
-# This p-value at least describes something in the realm of possibility, but it's still below
-# the threshold of what we would consider likely. There's only a 0.6% chance that our observed party
-# alignments came from this mixed distribution. This makes sense given that our sample size isn't that
-# large, and, even if it were, we're analyzing such an abstract, subjective set of values that modeling is
-# naturally very difficult.
+graphing$corr_scatterplot(table,
+  "V6_Scale",
+  "V8_Scale",
+  x_label = "Left   —   Social Ideology   —   Right",
+  y_label = "Pluralist      —      Populist"
+)
+# !!! ADDITIONAL POINT #5
+# !!! ADDITIONAL POINT #8
